284a023933af8874e308bdf15dde35a359ce3537
Ekaterina599 <82459656+Ekaterina599@users.noreply.github.com>
2022-11-22 14:39:38 +0600

    Update StudentPicker.kt

--- src/main/kotlin/component/StudentPicker.kt
+++ src/main/kotlin/component/StudentPicker.kt
@@ -1,12 +1,21 @@
 package component
 
+import data.Student
 import kotlinx.js.Record
 import kotlinx.js.get
+import org.w3c.dom.HTMLInputElement
+import org.w3c.dom.HTMLSelectElement
 import react.FC
 import react.Props
+import react.dom.html.ReactHTML
+import react.dom.html.ReactHTML.button
 import react.dom.html.ReactHTML.div
+import react.dom.html.ReactHTML.option
+import react.dom.html.ReactHTML.select
 import react.router.useParams
+import react.useRef
 import redux.*
+import tools.minus
 
 
 external interface StudentPickerProps : Props {
@@ -20,19 +29,16 @@ val CStudentPicker = FC<StudentPickerProps>("StudentPicker") { props ->
     val student = props.state.students.firstOrNull { it.id == studentName }
     if (student == null) {
         div {
-           // +"No course with name $studentName"
+            // +"No course with name $studentName"
         }
     } else {
         CCourseList {
             stud = student
             studId = student.id
             course = props.state.getCourseList(student)
-            clickCourseId = {courseId->
-                props.dispatch(MarkStudent(courseId, student.id))
-            }
-            gradeSet = {courseId, studentId, it->
-                props.dispatch(AddGrade(courseId, studentId, it))
-            }
+            allCourses = props.state.courses
+            gradeSet = { courseId, studentId, it -> props.dispatch(AddGrade(courseId, studentId, it)) }
+            addStudent = {course, studentId -> props.dispatch(AddStudentToCourse(course, studentId))}
         }
     }
 }
